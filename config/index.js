fcadad3c28ef6e580f3e4a21d01c00433998d188
Xavier <xavierwhite1@hotmail.com>
2019-05-26 13:52:10 +1000

    4

--- config/index.js
+++ config/index.js
@@ -1,19 +1,19 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-  siteTitle: 'Jodie', // Navigation and Site Title
-  siteTitleAlt: 'Jodie - Gatsby Starter Portfolio', // Alternative Site title for SEO
-  siteTitleShort: 'Jodie', // short_name for manifest
+  siteTitle: 'Xavier White Photography', // Navigation and Site Title
+  siteTitleAlt: 'Xavier White Photography - Portfolio', // Alternative Site title for SEO
+  siteTitleShort: 'Xavier', // short_name for manifest
   siteHeadline: 'Come & Enjoy our excellent photos', // Headline for schema.org JSONLD
-  siteUrl: 'https://jodie.lekoarts.de', // Domain of your site. No trailing slash!
+  siteUrl: 'https://elegant-hamilton-c04af5.netlify.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo.png', // Used for SEO and manifest
-  siteDescription: 'Image-heavy photography portfolio with colorful accents & great typography',
-  author: 'LekoArts', // Author for schema.org JSONLD
+  siteDescription: 'Xavier White Photography - Portfolio',
+  author: 'Xavierwhitephotography', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
   userTwitter: '@jodie', // Twitter Username
-  ogSiteName: 'jodie', // Facebook Site Name
+  ogSiteName: 'xavierwhitephotography', // Facebook Site Name
   ogLanguage: 'en_US', // og:language
   googleAnalyticsID: 'UA-XXXXXX-X',
 
